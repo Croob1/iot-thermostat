aade9deb24e764e374390d51291b573579fa12bb
Austin <austin@sleepybubble.com>
2025-12-08 17:36:11 +0000

    LED added now. Script functions with single target temperature

--- thermostat-controller.py
+++ thermostat-controller.py
@@ -1,16 +1,28 @@
+import RPi.GPIO as GPIO
 import Adafruit_DHT
 import time
 
 SENSOR_TYPE = Adafruit_DHT.DHT11
-GPIO_PIN_NUMBER = 4
+PIN_SENSOR = 4
+PIN_HEATER = 21
+targetTemp = 21
 
-if __name__ == "__main__" :
-  start_time = time.time()
-  humidity, temperature = Adafruit_DHT.read_retry(
-    SENSOR_TYPE, 
-    GPIO_PIN_NUMBER
-  )
-  end_time = time.time()
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_HEATER, GPIO.OUT)
 
-  print(f"Measured Temp={temperature}°C | Hum={humidity}%")
-  print(f"Measurement took {end_time-start_time}s")
+def sensor_read():
+    humidity, temperature = Adafruit_DHT.read_retry(SENSOR_TYPE, PIN_SENSOR)
+    return humidity, temperature
+
+def heater(on):
+    GPIO.output(PIN_HEATER, on)
+
+while True:
+    humidity, temperature = sensor_read()
+    if temperature < targetTemp:
+        heater(True)
+        print(f"Temp={temperature}, Target={targetTemp}, HeaterOn=True")
+    else:
+        heater(False)
+        print(f"Temp={temperature}, Target={targetTemp}, HeaterOn=False")
