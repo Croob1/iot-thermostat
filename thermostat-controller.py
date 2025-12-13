239c42f411b169607e89a363ccaadfa74d4e2667
Austin <austin@sleepybubble.com>
2025-12-13 15:29:34 +0000

    Script now connects to remote MQTT broker to send data. Config file reformatted to JSON

--- thermostat-controller.py
+++ thermostat-controller.py
@@ -3,12 +3,15 @@ import Adafruit_DHT
 import time
 import datetime
 import sys
+import paho.mqtt.client
+import json
 
 SENSOR_TYPE = Adafruit_DHT.DHT11
 PIN_SENSOR = 4
 PIN_HEATER = 21
 targetTemps = [0, 0, 0, 0, 0, 0, 0, 21, 21, 21, 0, 0, 0, 0, 0, 0, 0, 21, 21, 21, 0, 0, 0, 0]
 targetTemp = 0
+address, port, username, password = "127.0.0.1", 1886, "user", "pass"
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -29,32 +32,55 @@ def sensor_read():
 def heater(on):
     GPIO.output(PIN_HEATER, on)
 
-def load_targets():
-    try:
-        with open("config.txt", "r") as file:
-            for i in range(23):
-                try:
-                    targetTemps[i] = int(file.readline())
-                except Exception:
-                    print("Error: reading config.txt contents failed, please ensure the file is correctly formatted and try again.")
-                    log(e)
-                    sys.exit()
-    except Exception as e:
-        print("Error: config.txt not found. Generating file with default parameters")
-        log(e)
-        save_targets()
+def load_config():
+    global address, port, username, password
 
-def save_targets():
     try:
-        with open("config.txt", "w") as file:
-            for i in range(23):
-                file.write(str(targetTemps[i]) + "\n")
-    except Exception as e:
-        print("Error: writing to config.txt failed, please check process permissions and try again")
-        log(e)
+        file = open("config.json", "r") 
+    except Exception as err:
+        i = input("Error: cannot open config.json. If this is a first time execution press (y) to generate a default config file: ")
+        if i == "y":
+            save_config()
+            print("Generated config.json, edit this file and try again.")
+            sys.exit()
+        else:
+            print("Error code has been logged")
+            log(err)
+            sys.exit()
+
+    try:
+        with file:
+            contents = json.load(file)
+            targetTemps = contents["targets"]
+            address, port, username, password = contents["address"], contents["port"], contents["username"], contents["password"]
+    except Exception as err:
+        print("Error: reading config.json contents failed, please ensure the file is correctly formatted and try again.")
+        log(err)
         sys.exit()
 
-load_targets()
+def save_config():
+    try:
+        with open("config.json", "w") as file:
+            contents = {"address": address, "port": port, "username": username, "password": password, "targets": targetTemps}
+            json.dump(contents, file, indent=4)
+    except Exception as e:
+        print("Error: writing to config.json failed, please check process permissions and try again")
+        log(err)
+        sys.exit()
+
+load_config()
+
+try:
+    client = paho.mqtt.client.Client(paho.mqtt.client.CallbackAPIVersion.VERSION2, client_id="pi", userdata=None, protocol=paho.mqtt.client.MQTTv5)
+    client.username_pw_set(username, password)
+    client.connect(address, port)
+
+    client.loop_start()
+except Exception as err:
+    log(err)
+    print("Error: connection to MQTT broker failed")
+    sys.exit()
+
 
 while True:
     humidity, temperature = sensor_read()
@@ -66,8 +92,12 @@ while True:
     if temperature < targetTemp:
         heater(True)
         print(f"Temp={temperature}, Target={targetTemp}, HeaterOn=True")
+        client.publish("temperature", payload=temperature, qos=1)
     else:
         heater(False)
         print(f"Temp={temperature}, Target={targetTemp}, HeaterOn=False")
+        client.publish("temperature", payload=temperature, qos=1)
 
     time.sleep(5)
+
+client.loop_stop()
